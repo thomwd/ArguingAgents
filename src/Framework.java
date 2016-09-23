7616ed9b9487ec88f7f8eb21546afc0917f62eca
Thomas <thomwd@hotmail.com>
2016-09-23 14:22:22 +0200

    Added new classes and some small changes

---
+++ src/Framework.java
@@ -0,0 +1,19 @@
+
+public class Framework {
+	private  int frameworkId;
+	private  String text;
+	private  String textWt;
+
+
+	public  void addFramework(String frameworkId,String text,String textWt){
+		this.frameworkId = frameworkId;
+	}
+
+	public String getframeworkId() {
+		return frameworkId;
+	}
+
+
+
+
+}
