b3699ddf91f1db8bf6ae131976abc900be36a37e
Yannik Beckers <yyannikb@gmail.com>
2016-09-23 16:25:30 +0200

    Updated Agent, Argument, and Framework, especially their field types/names

--- src/Agent.java
+++ src/Agent.java
@@ -2,14 +2,15 @@
 public class Agent {
 	private  int agentId;
 	private  String name;
-	private  ArrayList arguments;
+	private  ArrayList<Argument> arguments;
 	private double credibility;
 
-	public  void addSource(int agentId,String name, String[] arguments){
-		setName(name);
-    this.agentId = agentId;
-		this.credibility = 1.0;
-	}
+	// Yannik: I think this can be deleted - if we should never want to change an agent (except his credibility value maybe)
+	// public  void addSource(int agentId,String name, String[] arguments){
+	// 	setName(name);
+    // this.agentId = agentId;
+	// 	this.credibility = 1.0;
+	// }
 
 	public String getName() {
 		return name;
@@ -24,19 +25,17 @@ public class Agent {
 	}
 
 	public void setCredibility(double credibility){
-		if(credibility>1||credibility<0){
+		if(credibility>1||credibility<0){		// TODO: Yannik: I would let credibility be between 0 and 2 or 3 maybe, where 1 is the default value. Or just impose cred >= 0
 			System.out.println("Credibility has to be between 0 and 1");
 		}else{
 			this.credibility = credibility;
 		}
 	}
 
-	public ArrayList getArguments(){
+	public ArrayList<Argument> getArguments(){
 		return arguments;
 	}
 
-	public void addArgument(int argumentId){
-		this.arguments.add(argumentId);
+	public void addArgument(Argument argument){	
+		this.arguments.add(argument);
 	}
-
-}
--- src/Framework.java
+++ src/Framework.java
@@ -1,5 +1,6 @@
 /**
- * Represents a complete argumentation framework
+ * Represents a complete argumentation framework, (a directed graph)
+ * If we want to load a new framework, we create a new framework object and discard the existing one
  */
 public class Framework {
 	private  String topicDescription;		// what the argumentation is about 
@@ -10,6 +11,10 @@ public class Framework {
 											// and between arguments and relations
 										
 	// TODO
+	// constructors/getters/setters
 	// methods:
-	// solve
+	// evaluate(mode, threshold)	evaluates the whole Argumentation (i.e. argument activity is calculated). 
+									// where mode specifies which proof standard we use etc, threshold specifies the activity level below which we will call an argument 'inactive'
+									//
+	// reset()						reset the arguments that we calculated so we can start from scratch/use a different mode for calculate
 }
